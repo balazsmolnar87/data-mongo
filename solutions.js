10c74289f3b406a428960ae60acb7c6add32ef42
balazsmolnar87 <94466198+balazsmolnar87@users.noreply.github.com>
2022-10-19 14:10:14 +0200

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -7,6 +7,11 @@
  * What are the top 3 most selling products (based on sold quantity)?
  */
 
+db.orders.aggregate([
+    {"$group": {"_id": "$product", "quantities": {"$sum": "$quantity"}}},
+    {"$sort": {"quantities": -1}},
+    {"$limit": 3}
+  ]);
 
 /**
  * Which city sold the most products?
